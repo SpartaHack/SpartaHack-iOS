6270f0fa98dfe33104557f7e2dc32689c288509b
w2huy <huynhwi2@msu.edu>
2019-08-06 05:32:56 -0400

    countdown timer is functional

--- SpartahackVI-iOS/Schedule/ScheduleViewController.swift
+++ SpartahackVI-iOS/Schedule/ScheduleViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     
     @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var schedule:[Schedule]?
-    
-    @IBOutlet weak var tableView: UITableView!
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,7 +57,8 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
                 print(err.localizedDescription)
             }
         }.resume()
-        
+    
+        runTimer()
     }
 
     
@@ -67,10 +69,65 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "scheduleCell")
         
-        cell.textLabel?.text = schedule?[indexPath.item].title
+        let event = schedule?[indexPath.item].title
+        let timeOf = formatTime(scheduledTime: (schedule?[indexPath.item].time)!)
+        cell.textLabel?.text = "\(timeOf) - \(event ?? "(-_-)")"
         cell.textLabel?.font = .systemFont(ofSize: 14, weight: UIFont.Weight.regular)
-        
+        cell.selectionStyle = .none
         return cell
     }
     
+    func formatTime(scheduledTime: String) -> String {
+        
+        let string = scheduledTime
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        let date = dateFormatter.date(from: string)!
+        dateFormatter.dateFormat = "HH:mm"
+        
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+    
+    @objc func countdown() {
+        // here we set the current date
+        
+        let date = NSDate()
+        let calendar = Calendar.current
+        
+        let components = calendar.dateComponents([.hour, .minute, .month, .year, .day, .second], from: date as Date)
+        
+        let currentDate = calendar.date(from: components)
+        
+        let userCalendar = Calendar.current
+        
+        // here we set the due date. When the timer is supposed to finish
+        let competitionDate = NSDateComponents()
+        competitionDate.year = 2019
+        competitionDate.month = 8
+        competitionDate.day = 7
+        competitionDate.hour = 00
+        competitionDate.minute = 00
+        competitionDate.second = 00
+        let competitionDay = userCalendar.date(from: competitionDate as DateComponents)!
+        
+        //here we change the seconds to hours,minutes and days
+        let CompetitionDayDifference = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate!, to: competitionDay)
+        
+        
+        //finally, here we set the variable to our remaining time
+        let daysLeft = CompetitionDayDifference.day
+        let hoursLeft = CompetitionDayDifference.hour
+        let minutesLeft = CompetitionDayDifference.minute
+        let secondsLeft = CompetitionDayDifference.second
+        
+        //Set countdown label text
+        countdownLabel.text = "\(daysLeft ?? 0) : \(hoursLeft ?? 0) : \(minutesLeft ?? 0) : \(secondsLeft ?? 0)"
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(countdown)), userInfo: nil, repeats: true)
+    }
 }
